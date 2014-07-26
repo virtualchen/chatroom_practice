32cb5edd1628f6f71e1a9b8b866c77c861261cc5
virtualchen <virtualchen@gmail.com>
2014-07-26 21:53:14 +0800

    first commit

---
+++ jquery.websocket.js
@@ -0,0 +1,48 @@
+(function($) {
+
+// WebSocket object
+var ws;
+
+// The Div element selected by jQuery selector
+var div = this;
+
+function onWsMessage(message) {
+   var json = JSON.parse(message.data);
+	console.log(json);
+
+   if (json.type === 'message') {
+     content.append('<div class="panel panel-primary">' +
+				'<div class="panel-heading">Say</div>' +
+				'<div class="panel-body">' + json.data.message + '</div>' +
+				'</div>');
+   }
+}
+
+$.fn.receiveWebSocket = function () {
+     content = this;
+
+     ws.onmessage = onWsMessage;
+};
+
+$.fn.createWebSocket = function () {
+  if ("WebSocket" in window)
+  {
+     // Let us open a web socket
+     ws = new WebSocket("ws://localhost:8080/start", ['echo-protocol']);
+     ws.onopen = function() {
+	     //div.append("<h2>Done</h2>");
+     };
+
+     ws.onclose = function() { 
+        // websocket is closed.
+     };
+     ws.onerror = function() { 
+        div.html("<h1>error</h1>");
+     };
+  } else {
+     // The browser doesn't support WebSocket
+     alert("WebSocket NOT supported by your Browser!");
+  }
+};
+
+})($);
